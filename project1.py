9cee8c81b219a3ee58f76f9568f1899eaddeb860
Gamer4lifeovi <ukporheo@gmail.com>
2025-09-02 14:37:12 +0100

    commit

--- project1.py
+++ project1.py
@@ -1,10 +1,13 @@
 name = input("Enter your name")
 print ("Hello" + name + "Welcome to the game")
 
-should_we_play = input("Do you want to play")
+should_we_play = input("Do you want to play").lower()
 
 if should_we_play == "yes" :
     print("we are going to play")
 
-if should_we_play != "no" :
+elif should_we_play == "YES" :
+    print("WE ARE GOING TO PLAY")
+
+else:
     print("Goodbye")
