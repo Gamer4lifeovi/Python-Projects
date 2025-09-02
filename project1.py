fb0d6824ba0c3ddefe5262dc76f0f4f1b004cd35
Gamer4lifeovi <ukporheo@gmail.com>
2025-09-02 13:18:03 +0100

    change

---
+++ project1.py
@@ -0,0 +1,10 @@
+name = input("Enter your name")
+print ("Hello" + name + "Welcome to the game")
+
+should_we_play = input("Do you want to play")
+
+if should_we_play == "yes" :
+    print("we are going to play")
+
+if should_we_play != "no" :
+    print("Goodbye")
