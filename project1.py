310b210a40d2028e272925ad89c133ce58768f16
Gamer4lifeovi <ukporheo@gmail.com>
2025-09-02 18:35:23 +0100

    New commit

--- project1.py
+++ project1.py
@@ -5,6 +5,31 @@ should_we_play = input("Do you want to play").lower()
 
 if should_we_play == "y" or should_we_play == "yes" :
     print("we are going to play")
+    weapon = input("Choice of weapon (sword/axe)").lower()
 
+    if weapon == "sword" :
+        print("Sword added to your backpack")
+
+    elif weapon == "axe" :
+        print("Axe added to bapack")
+
+    direction = input("Do you want to go left or right? (Left/Right)").lower()
+    if direction == "left" :
+        print("Okay you went left and fell of a bridge, game over,Try again")
+        quit()
+        
+
+    elif direction == "right":
+        print("Okay we are going right")
+        choice = input("Okay now you see a bridge do you want to cross it or go under it")
+
+        if choice == "swim" :
+            print("You got eaten by a shark, you die,Try again")
+            quit()
+            
+
+    else:
+        print("Sorry not a valid answer, you die")
 else:
     print("Goodbye")
+    quit()
