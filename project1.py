464a4e74a2a7762c40a6c0a15cf45aed7ef458f5
Gamer4lifeovi <ukporheo@gmail.com>
2025-09-03 23:13:46 +0100

    New Commit

--- project1.py
+++ project1.py
@@ -19,6 +19,7 @@ if should_we_play == "y" or should_we_play == "yes" :
     direction = input("Do you want to go left or right? (Left/Right)").lower()
     if direction == "left" and weapon == "axe":
         print("Lucky u, you were able to hang on")
+        choice = input("Okay now you see a bridge do you want to cross it or swim under it (Cross it/ Swim)").lower()
     
     elif direction == "left" :
         print("Sorry you fell of")
@@ -37,12 +38,24 @@ if should_we_play == "y" or should_we_play == "yes" :
             print("Sorry, You got eaten by a shark, Want to try again")
         
 
-        elif choice == "cross it":
+        elif choice == "cross it" or "crossit" :
             print("Great choice u made it safely")
             next1 = input("Okay now do u want to go straight forward or  move toward the left (Straight/left)").lower()
 
             if next1 == "straight":
                 print("Glad you could make it,  u beat the game this is the gold")
+                quit()
+
+            elif next1 == "left" and weapon == "none":
+                print("U barely missed the boulder because you were not equipped with a weapon u were light enough to dodge the huge boulder")
+                print("U beat the game this is the gold, Bye")
+                quit()
+
+            elif next1 == "left" :
+                print("U were so close such a wrong turn, You lose, Try again")
+
+            else :
+                print("Wrong answer")
 
 
     else:
