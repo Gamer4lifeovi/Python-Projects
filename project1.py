10c80f6fa366b9d5ab224a9f8c33281432a40a19
Gamer4lifeovi <ukporheo@gmail.com>
2025-09-07 21:33:00 +0100

    New Commit

--- project1.py
+++ project1.py
@@ -1,11 +1,18 @@
 name = input("Enter your name ")
+
+name = name.strip()
+
 print ("Hello " + name + " Welcome to the game")
 
 should_we_play = input("Do you want to play").lower()
 
+should_we_play = should_we_play.strip()
+
 if should_we_play == "y" or should_we_play == "yes" :
     print("we are going to play")
     weapon = input("Choice of weapon (sword/axe/none)").lower()
+
+    weapon = weapon.strip()
 
     if weapon == "sword" :
         print("Sword added to your backpack")
@@ -17,6 +24,10 @@ if should_we_play == "y" or should_we_play == "yes" :
         print("No wepon added to backpack")
 
     direction = input("Do you want to go left or right? (Left/Right)").lower()
+
+    direction = direction.strip
+
+
     if direction == "left" and weapon == "axe":
         print("Lucky u, you were able to hang on")
         choice = input("Okay now you see a bridge do you want to cross it or swim under it (Cross it/ Swim)").lower()
@@ -30,6 +41,9 @@ if should_we_play == "y" or should_we_play == "yes" :
         choice = input("Okay now you see a bridge do you want to cross it or swim under it (Cross it/ Swim)").lower()
 
         if choice == "swim" and weapon == "sword":
+
+            choice=choice.stip()
+
             print("Lucky you, You killed the shark")
             next1 = input("Okay now do u want to go straight forward or  move toward the left (Straight/left)").lower()
 
@@ -41,6 +55,8 @@ if should_we_play == "y" or should_we_play == "yes" :
         elif choice == "cross it" or "crossit" :
             print("Great choice u made it safely")
             next1 = input("Okay now do u want to go straight forward or  move toward the left (Straight/left)").lower()
+
+            next1 = next1.strip()
 
             if next1 == "straight":
                 print("Glad you could make it,  u beat the game this is the gold")
